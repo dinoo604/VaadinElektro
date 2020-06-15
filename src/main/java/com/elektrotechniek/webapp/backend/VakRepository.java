ef7768549debf3ced6cd0cbb417c8a97efa9e878
dinesh <dinharkhoe@gmail.com>
2020-06-15 11:25:58 -0300

    initial commit

---
+++ src/main/java/com/elektrotechniek/webapp/backend/VakRepository.java
@@ -0,0 +1,7 @@
+package com.elektrotechniek.webapp.backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VakRepository extends JpaRepository<Vak, Long> {
+
+}
